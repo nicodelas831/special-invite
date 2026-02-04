fd4a9a18061f92d1b0a1b0e3dafe1a68726d62b9
nicodelas831 <nicholas97831@gmail.com>
2026-02-04 22:34:37 +0800

    script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * (window.innerWidth - noBtn.clientWidth);
+  const y = Math.random() * (window.innerHeight - noBtn.clientHeight);
+
+  noBtn.style.position = "absolute";
+  noBtn.style.left = `${x}px`;
+  noBtn.style.top = `${y}px`;
+});
+
+yesBtn.addEventListener("click", () => {
+  document.body.innerHTML = "<h1 style='text-align:center;margin-top:20%;font-size:40px;'>YAY!! 💖 Can't wait for our date!</h1>";
+});
